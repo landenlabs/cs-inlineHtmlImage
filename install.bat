eb0170360f245193adf520a5601df31f051557c1
landenlabs <landenlabs@gmail.com>
2025-01-02 09:23:11 -0500

    fix install

--- install.bat
+++ install.bat
@@ -6,12 +6,15 @@ set prog=InlineHtmlImages
 set bindir=d:\opt\bin
 set msbuild=F:\opt\VisualStudio\2022\Preview\MSBuild\Current\Bin\MSBuild.exe
 
- 
+cd %prog%
+dir
+
 @echo ---- Clean Release %prog% 
 lldu -sum obj bin 
 rmdir /s obj  2> nul
 rmdir /s bin  2> nul
 @rem %msbuild% %prog%.sln  -t:Clean
+cd ..
 
 @echo.
 @echo ---- Build Release %prog% 
@@ -19,17 +22,19 @@ rmdir /s bin  2> nul
 
 @echo.
 @echo ---- Build done 
-if not exist "bin\x64\Release\%prog%.exe" (
-   echo Failed to build bin\x64\Release\%prog%.exe
+if not exist "%prog%\bin\x64\Release\%prog%.exe" (
+   echo Failed to %prog%\build bin\x64\Release\%prog%.exe
+   dir %prog%\bin\x64\Release
    goto _end
 )
  
 @echo ---- Copy Release to c:\opt\bin2
-copy  bin\x64\Release\%prog%.exe %bindir%\%prog%.exe
-dir   bin\x64\Release\%prog%.exe %bindir%\%prog%.exe
+copy  %prog%\bin\x64\Release\%prog%.exe %bindir%\%prog%.exe
+dir   %prog%\bin\x64\Release\%prog%.exe %bindir%\%prog%.exe
 
 @rem play happy tone
 rundll32.exe cmdext.dll,MessageBeepStub
 rundll32 user32.dll,MessageBeep
  
 :_end
+ 
